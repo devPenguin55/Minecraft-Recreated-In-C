4b5dfdbe5c40aa8841a68d9245d2eaffc63fd738
devPenguin55 <aaravdeshmane@gmail.com>
2026-02-08 01:55:01 -0700

    removed block breaking extra speedup in render.c

--- render.c
+++ render.c
@@ -119,10 +119,7 @@ void spinObject()
     T = T + 0.00025;
     if (T > 360)
         T = 0;
-
-    if (userBlockBreakingTimeElapsed >= 1) {
-        userBlockBreakingTimeElapsed++;
-    }
+        
     glutPostRedisplay();
 }
 
